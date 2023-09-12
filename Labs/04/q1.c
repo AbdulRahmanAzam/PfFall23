6078eb6293c2b79bf0b046e2f3bd7af8a1472ea6
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-09-12 21:16:14 +0500

    Update q1.c

--- Labs/04/q1.c
+++ Labs/04/q1.c
@@ -11,14 +11,23 @@
 
 #include <stdio.h>
 
-int main(){
-	 int i = 1; //counter variable for while loop
+int main() {
+	int n, i=0, j=0; //initialize n, i and j
+	printf("Input any number 'n': ");
+	scanf("%d", &n);
 
-	printf("Please enter a whole number greater than 0 to printing counting upto: ")
-	scanf("%d ", &n);
+	while (i < n){
+		j = 0;
+		printf("\n"); 
+		
+		while (j <= i) {
+			printf("*");
+			j = j+1;
+		};//end while loop for *	
+		
+		i = i+1;
+	};//end while loop for line break
 
-	while(i <= n){
-	printf("* \n");
-	};//end while
-}//end main
+}// end main
+
 
