8960177240f3f71b83a13ee3779865ae34370e87
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-09-12 10:57:02 +0500

    Create q2.c

---
+++ Labs/04/q2.c
@@ -0,0 +1,26 @@
+/*
+*programmer : Abdul Rahman Azam
+*Date: 12/sep/2023
+*Descrition: 
+*related Files: 
+*/
+
+//--Include Files--//
+
+//--Global Variables--//
+
+#include <stdio.h>
+
+int main(){
+	int i;
+	int j;
+	for(j = 1; j < 5; j = j+1){	
+	
+	printf("\n");	
+
+	for(i = 1; i< 5; i = i+1 ){
+	printf("*");
+	};//end loop
+	
+	};//for line change
+}//end main
