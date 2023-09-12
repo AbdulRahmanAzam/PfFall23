57a95c99c8d3f71af021166bbc567e5a7bf8eebc
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-09-12 21:36:27 +0500

    Update q4.c

--- Labs/04/q4.c
+++ Labs/04/q4.c
@@ -1,8 +1,8 @@
 /*
 *programmer : Abdul Rahman Azam
 *Date: 12/sep/2023
-*Descrition: 
-*related Files: 
+*Descrition: Using Quadratic formula to give the answer
+*Related Files: 
 */
 
 //--Include Files--//
@@ -13,22 +13,22 @@
 #include <math.h>
 
 int main(){
-int a, b , c;
-int root1, root2, undersquare;
-	printf("Enter the value of x square");
-	scanf("%d", &a);
+int a, b , c; //initializing a b c
+int root1, root2, undersquare; //root1 and root 2 for answers and undersquare is extra to calculate
+	printf("Enter the value of x square = ");
+	scanf("%d", &a);// ask value of x  square
 
-	printf("Enter the value of x");
+	printf("Enter the value of x = ");// ask value of x  
 	scanf("%d",&b);
 
-	printf("Enter the value of last constant");
+	printf("Enter the value of last constant = ");// ask value of constant
 	scanf("%d",&c);
 
 	undersquare = sqrt(b *b - 4 *a *c);
-	root1 = (-b + undersquare)/2 ;
+	root1 = (-b + undersquare)/2 ;//calculations
 	root2 = (-b - undersquare)/2;
 
-	 printf("%d",root1);
-	 printf("%d",root2);		
+	 printf("x = %d\n",root1);
+	 printf("x = %d",root2);		
 
 }//end main
