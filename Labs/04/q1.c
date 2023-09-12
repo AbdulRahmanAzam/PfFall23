fabee73b655f66714b7e33d3836f4d52368e90c0
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-09-12 10:56:11 +0500

    Create q1.c

---
+++ Labs/04/q1.c
@@ -0,0 +1,24 @@
+/*
+*programmer : Abdul Rahman Azam
+*Date: 12/sep/2023
+*Descrition: 
+*related Files:
+*/
+
+//--Include Files--//
+
+//--Global Variables--//
+
+#include <stdio.h>
+
+int main(){
+	 int i = 1; //counter variable for while loop
+
+	printf("Please enter a whole number greater than 0 to printing counting upto: ")
+	scanf("%d ", &n);
+
+	while(i <= n){
+	printf("* \n");
+	};//end while
+}//end main
+
