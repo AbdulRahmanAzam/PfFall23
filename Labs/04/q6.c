fb1473fbed3f725567eb742d797d49e2f18ace35
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-09-18 00:48:30 +0500

    Update q6.c

--- Labs/04/q6.c
+++ Labs/04/q6.c
@@ -1,1 +1,32 @@
 
+#include<stdio.h>
+
+int main() {
+    int i=0, j =1,k=0, n =3,m =1,o =1;
+    printf("Enter value of n ");
+    scanf("%d",&n);
+    m=n;
+    o=n;
+    n= n*2 -1;
+    
+    for(i=1; i<=n;i++){
+        for(j=1;j<=n;j++){
+            if(j ==m || j==o ){
+                printf("*");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        if(i >=4){
+         m = m+1;
+         o=o-1;
+        }
+        else{
+        m=m-1;
+        o=o+1;
+        }
+        printf("\n");
+    }
+   
+}//end main
