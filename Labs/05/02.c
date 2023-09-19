08ce90a8cbeac179356c5f9504a288843ba78774
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-09-19 09:56:45 +0500

    Create 02.c

---
+++ Labs/05/02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	int time;
+	printf("Enter time in 24 hour format ");
+	scanf("%d", &time);
+	
+	if(time >5 && time < 11){
+		printf("Good Morning");
+	}
+	else if(time > 12 && time < 18){
+		printf("Good Evening");
+	}
+	else if( time> 18 &&  time < 24){
+		printf("Good Night");
+	}
+}
