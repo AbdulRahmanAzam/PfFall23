6306614fd4e5a7517faf7555f17b6210a70322fe
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-09-19 18:39:36 +0500

    Update 04.c

--- Labs/05/04.c
+++ Labs/05/04.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main() {
+    char oper;
+    int a,b;
+
+    printf("Enter the operator ");
+    scanf("%c", &oper);
+    
+    printf("Enter the first operand ");// display and in input first operand
+    scanf("%d", &a);
+    
+    printf("Enter the second operand ");// display and in input second operand
+    scanf("%d", &b);
+    
+    switch(oper){
+    case '+':
+    printf("%d", a+ b);
+    break;
+    case '-':
+    printf("%d", a-b);
+    break;
+    case '*':
+    printf("%d", a*b);
+    break;
+    case '/':
+    printf("%d", a/b);
+    break;
+    default:
+    printf("you entered wrong operator");
+    }
+}//end main
