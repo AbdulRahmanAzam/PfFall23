046a6ba362bca901ed0a6fec91f38d868292cc90
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-09-19 19:22:18 +0500

    Create 08.c

---
+++ Labs/05/08.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int item, b, c;
+    printf("Enter the value of each item");
+    scanf("%d", &item);
+    
+    printf("Enter the value of ");
+    // scanf("");
+    
+    if(item < 2000){
+        item = item (item * 0.5);
+        printf("");
+    }
+}
