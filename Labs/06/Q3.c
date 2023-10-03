514fe86243beebd3c52fb0ab910b5b483730fb07
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-10-03 09:33:56 +0500

    Create Q3.c

---
+++ Labs/06/Q3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int i,sum=0,prev=0,a=0,b=1,total=0; //initializing variables
+    int fab = 10000;
+    for(i=1; sum <= fab; i++){
+        a = b;
+        b =sum;
+        sum = a+b;
+        if(sum < fab){
+            printf("%d\n",sum);
+        }
+        if(sum %3 ==0 || sum %5==0|| sum %7==0){ //check divisble by 3, 5 or 7
+            total = total + sum;
+        }
+        if(sum > fab){
+            break;
+        }
+    }//end i loop
+    printf("The sum is %d", total);
+    
+}//end main
