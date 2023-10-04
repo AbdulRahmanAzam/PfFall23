0792bc237a04ee1adffa4226ba1d0082ea1accd1
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-10-04 15:28:42 +0500

    Update 07.c

--- Labs/05/07.c
+++ Labs/05/07.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
-
 int main(){
-	int  cost =0, saved=0, discounted=0;
-	printf("Enter total cost of items");
-	scanf("%d", &cost);
-	
-	if(cost >= 2000){
-		saved = cost * 0.50;		
-	}
-printf("%d", saved);
-}//end main
+	int total, discount,actual;
+	printf("Enter the actual amount  ");
+	scanf("%d", &actual);
+    
+    if(actual > 2000 && actual <= 4000){
+        discount = actual * 0.2;
+    }else if(actual > 4000 && actual <= 6000 ){
+        discount = actual * 0.3;
+    }else if(actual >6000 ){
+        discount = actual * 0.5;
+    }
+    
+        total = actual - discount;
+    
+    printf("actual= %d\ntotal= %d\ndiscount = %d",actual,total, discount);
+    
+			
+}
