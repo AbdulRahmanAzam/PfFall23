dd9a9e00d4dbdf8fddb2ae437454014a70e53cfe
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-10-24 10:03:34 +0500

    Update Q2.c

--- Labs/08/Q2.c
+++ Labs/08/Q2.c
@@ -1,21 +1,33 @@
+/* 
+Name:Abdul Rahman Azam 
+Date: 24-oct-2023 
+Description: make a calculator using functions
+*/
 #include <stdio.h>
 int operate(int a, int b, char x){
     int i = 0;
-    if(x == '*'){
-        i = a *b;
-    }else if(x == '+'){
-        i = a +b;
-    }else if(x == '-'){
-        i = a - b;
-    }else if(x== '/'){
-        i = a/b;
-    }
+    
     printf("%d", i);
     return i;
 }//end operation
+int mul(int a, int b ){
+    return a *b;
+}
+int divide(int a, int b){
+    return a/b ;
+}
+int sum (int a , int b){
+    int n = a +b;
+    return n;
+}
+int minus(int a, int b){
+    int n = a -b; 
+    return n;
+}
 int main(){
-    int a,b;
-    char x = 0;
+    int a =2,b=5;
+    int n;
+    char x ;
     printf("Enter first num = ");
     scanf("%d", &a);
     
@@ -23,7 +35,18 @@ int main(){
     scanf("%d", &b);
     
     printf("Enter the operator = ");
-    scanf("%c", &x);
+    scanf(" %c", &x);
     
-    operate(a,b,x);
+    if(x == '*'){
+        n = mul(a,b);
+    }
+    else if(x == '+'){
+        n = sum(a,b);
+    }else if(x == '-'){
+        n = minus(a,b);
+    }else if(x== '/'){
+        n = divide(a,b);
+    }
+    
+    printf("%d ", n);
 }//end main
