f9e45aaf47ac39f7d635ac7ed9ebcac4c1715f46
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-10-24 10:21:43 +0500

    Update Q3.c

--- Labs/08/Q3.c
+++ Labs/08/Q3.c
@@ -4,19 +4,27 @@ Date: 8-oct-2023
 Description: count the letter 'a' in a string
   */
 #include <stdio.h>
-int count(char *str){
+#include <string.h>
+int count(char *str, int x){
     int a=0;
     for(int i =0; str[i] != '\0';i++){
-        if(str[i] == 'a' || str[i] == 'A'){
+        if(str[i] == x){
             a++;
         }
     }
     return a;
 }
 int main(){
-    char str[] = "Abdul Rahman Azam" ;
+    char str[100] ;
     int i;
-    // char *str[] = arr[];
-    i = count(str);
+    char x;
+    printf("Enter the name ");
+    // scanf(" %[^\]s ", str); // not working 
+    gets(str);
+    
+    printf("Enter the character ");
+    scanf(" %c", &x);
+    
+    i = count(str,x);
     printf("%d", i);
 }//end main
