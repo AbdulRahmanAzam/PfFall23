4c99445daafd301025c5715286ea440df6969e69
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-10-24 08:19:30 +0500

    Update Q1.c

--- Labs/08/Q1.c
+++ Labs/08/Q1.c
@@ -1,1 +1,28 @@
-
+/* 
+Name:Abdul Rahman Azam 
+Date: 8-oct-2023 
+Description: swap two integers
+  */
+#include <stdio.h>
+int swap(int a, int b){
+    // it is the type of formula for swapping two integers without using third variable
+    a = a - b;
+    b = a + b;
+    a = b - a;
+    // it is another type of formula for swapping two integers without using third variable
+    // a = a ^ b;
+    // b = a ^ b;
+    // a = a ^ b;
+    printf("num1 = %d\n num2 = %d ",a,b);
+}//end swap
+int main() {
+    int a,b;// iniitialization
+    printf("Enter the value of num1 = ");
+    scanf("%d" , &a);
+    
+    printf("\nEnter the value of num2 = ");
+    scanf("%d", &b);
+    
+    swap(a,b);
+    return 0;
+}//end main
