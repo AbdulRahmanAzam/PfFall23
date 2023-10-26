b2aa22f883a57a10427db929704a232a094aacc1
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-10-27 01:17:34 +0500

    Create Q5.c

---
+++ Assignments/assign/Q5.c
@@ -0,0 +1,20 @@
+/* ##Discription: find all ramanujan hardy number
+  */
+#include <stdio.h>
+int main(){
+    int n = 20;
+    int i,j,k,l;
+    
+    for(i=1;i< n*n;i++){
+        for(j=1;j<n*n;j++){
+            for(k=j+1;k<n*n;k++){
+                for(l=k+1;l<n*n;l++){
+                    if(i*i*i + j*j*j == k*k*k + l*l*l  ){
+                        printf("%d %d + %d %d == %d\n",i,j,k,l, i*i*i+j*j*j );
+                    }
+                }
+            }//end k 
+        }//end j
+    }//end i 
+    printf("done");
+}//end main
