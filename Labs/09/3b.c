9ff289045768259244ce3fcd6e7a445c589836ba
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-11-11 10:52:27 +0500

    Update 3b.c

--- Labs/09/3b.c
+++ Labs/09/3b.c
@@ -3,18 +3,16 @@ Name:Abdul Rahman Azam
 Date: 31-oct-2023 
 Description: print fabonacci series using recursion
 */
+
+
 #include <stdio.h>
-int fabon(int a,int b){
-    if(b>=5) return printf("%d",a+b);
-    
-    
-    printf("%d", a);
-      fabon(b, a+b) ;
-    return 1;
+int fabon(int a){
+    if(a<=1) return 1;
+    return fabon(a-1) + fabon(a-2);
 }
 int main(){
-    int n = 10;
-    
-    fabon(1,1);
-    // printf("%d", s);
+    int n = 20;
+    for(int i=0;i<n;i++){
+        printf("%d ",fabon(i));
+    }
 }
