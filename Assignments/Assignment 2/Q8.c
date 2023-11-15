c1be5c196fc89d9cc49d04dc92f08a1db01751a4
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-11-16 00:13:30 +0500

    Update Q8.c

--- Assignments/Assignment 2/Q8.c
+++ Assignments/Assignment 2/Q8.c
@@ -1,12 +1,17 @@
-Description : 
+/*Name : Abdul Rahman Azam 
+Roll no : 23k-0061
+Date : 3-Nov-2023
+Discription : Multiplyiing the number till it became zero by divide and give its persistence
+*/
 #include <stdio.h>
 int main(){
-    int n,a=1;
+    int n,a=1,per=0,check=0;
     do{
         printf("Enter the positive integer = ");
-        scanf(" %d ", &n);
-        if(n == 0) break;// when a string assign to integer it will become 0 automatically
+        scanf(" %d ", &check);
+        if(check == 0) break;// when a string assign to integer it will become 0 automatically
         printf("persistence");
+        n = check;
         a = n;
         while(a/10 >0 ){
             n = a;
@@ -14,12 +19,12 @@ int main(){
             while(n >0){
                 a *= n % 10;
                 n = n/10;
-                
             }
-            printf(" %d ", a);
+            per++;
         }
-        printf("\n\n");
-        
-    }while(n == 0 );
-    
+        //since the number divisible by 10 gives more than 1 so it must only be 1
+        if(check>10 && check%10==0) per =1;
+        printf(" %d\n\n ",per);
+        per=0;
+    }while(check != 0 );
 }//end main
