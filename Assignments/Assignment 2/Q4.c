f21477500e29bce885a27009c08e93da7281ac4b
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-11-19 19:54:00 +0500

    Update Q4.c

--- Assignments/Assignment 2/Q4.c
+++ Assignments/Assignment 2/Q4.c
@@ -1,132 +1,83 @@
-#include <stdio.h>
-void print(int arr[10][10]){
-    for(int i =0;i<5;i++){
-        for(int j=0;j<5;j++){
-            if(arr[i][j]==1) printf("%d",arr[i][j]);
-            else printf(" ");
-        }
-        printf("\n");
-    }
-}
-int mazetr(char maze[5][5],int vis[5][5] ,int a,int b){
-    
-    vis[a][b]=1;
-    // printf("yes");
-    if(maze[a][b+1]=='E'){
-        print(vis);
-        return;
-    }
-    
-    if(maze[a+1][b]=='O' && vis[a+1][b]==0){
-        mazetr(maze,vis,a+1,b);
-        
-    }
-    
-    if(maze[a][b+1]=='O'&& vis[a][b+1]==0){
-        mazetr(maze,vis,a,b+1);
-    }
-    
-    if(maze[a-1][b] =='O'&& vis[a-1][b]==0){
-        mazetr(maze,vis,a-1,b);
-    }
-    
-    if(maze[a][b-1] == 'O'&& vis[a][b-1]==0){
-        mazetr(maze,vis,a,b-1);
-    }
-    // vis[a][b]=0;
-}//end mazetraversal
+/*
+ * Creator: Abdul Rahman Azam
+ * Date: 3-Nov-2023
+ * Desc: path of a maze
+ */
 
-    // for(int i =0;i<5;i++){
-    //     for(int j=0;j<5;j++){
-    //         vis[i][j] = 0;       
-    //     }
-    // }
-int main(){
-char maze[5][5] = {
+#include <stdio.h>
+
+int create_path(char maze[5][5], int cx, int cy) 
+{
+    // base case
+    if (maze[cx][cy] == 'E') {
+    	maze[cx][cy] = '1';
+    	return 1;
+    }
+
+    maze[cx][cy] = '1'; // mark traversed path
+
+    // right
+    if (cy + 1 < 5 && maze[cx][cy+1] != 'W' && maze[cx][cy+1] != '1')
+        if (create_path(maze, cx, cy+1)) return 1;
+
+    // down
+    if (cx+1 < 5 && maze[cx+1][cy] != 'W' && maze[cx + 1][cy] != '1')
+        if (create_path(maze, cx + 1, cy)) return 1;
+
+    // go back
+    maze[cx][cy] = 'O';
+    return 0;
+} // create_path(char[][5], int, int)
+
+void show_path(char maze[5][5]) 
+{
+	// show points where marked as visited
+	for (int i = 0; i < 5; i++) {
+	    for (int j = 0; j < 5; j++) {
+	        if (maze[i][j] == '1') printf("%d,%d  ", i, j);
+	    }
+	}
+} // show_path(char[][5])
+
+void show_maze(char maze[5][5]) {
+    int start[2], end[2];
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			printf("%c ", maze[i][j]);
+			if (maze[i][j] == 'S') start[0] = i, start[1] = j;
+			if (maze[i][j] == 'E') end[0] = i, end[1] = j;
+		}
+		printf("\n");
+	}
+	printf("start at: %d,%d\nend at: %d,%d\n", start[0], start[1], end[0], end[1]);
+} // show_maze(char[][5])
+
+int main()
+{
+	int cx, cy;
+	char maze[5][5] = {
  {'S', 'O', 'O', 'W', 'W'},
  {'O', 'W', 'O', 'O', 'W'},
- {'O', 'W', 'O', 'W', 'O'},
  {'W', 'W', 'O', 'W', 'O'},
+ {'W', 'O', 'O', 'W', 'O'},
  {'W', 'W', 'O', 'E', 'W'} 
 };
-int x,y;
-for(int i=0;i<5;i++){
-    for(int j=0;j<5;j++){
-        if(maze[i][j] == 'S'){
-             x=i;
-         y=j;
-        }
-    }
-}
-int vis[5][5] = {0};
-mazetr(maze,vis,x,y);
 
-}//end main
+	// input maze elements
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (maze[i][j] == 'S') {
+				cx = i;
+				cy = j;
+			}
+		}
+	}
 
-
-//below is code of CHATGPT
-
-// #include <stdio.h>
-
-// void print(int arr[10][10]) {
-//     for (int i = 0; i < 5; i++) {
-//         for (int j = 0; j < 5; j++) {
-//             if (arr[i][j])
-//                 printf("%d", arr[i][j]);
-//             else
-//                 printf(" ");
-//         }
-//         printf("\n");
-//     }
-// }
-
-// void mazetr(char maze[5][5], int vis[10][10], int a, int b) {
-//     vis[a][b] = 1;
-//     // printf("Visited cell (%d, %d)\n", a, b);
-
-//     if (maze[a][b + 1] == 'E') {
-//         print(vis);
-//         return;
-//     }
-
-//     if (maze[a + 1][b] == 'O' && !vis[a + 1][b]) {
-//         mazetr(maze, vis, a + 1, b);
-//     }
-
-//     if (maze[a][b + 1] == 'O' && !vis[a][b + 1]) {
-//         mazetr(maze, vis, a, b + 1);
-//     }
-
-//     if (maze[a - 1][b] == 'O' && !vis[a - 1][b]) {
-//         mazetr(maze, vis, a - 1, b);
-//     }
-
-//     if (maze[a][b - 1] == 'O' && !vis[a][b - 1]) {
-//         mazetr(maze, vis, a, b - 1);
-//     }
-// }
-
-// int main() {
-//     char maze[5][5] = {
-//         {'S', 'O', 'O', 'W', 'W'},
-//         {'O', 'W', 'O', 'O', 'W'},
-//         {'O', 'W', 'O', 'W', 'O'},
-//         {'W', 'W', 'O', 'W', 'O'},
-//         {'W', 'W', 'O', 'E', 'W'}};
-
-//     int vis[10][10] = {0}; // Initialize the vis array with 0
-
-//     int x, y;
-//     for (int i = 0; i < 5; i++) {
-//         for (int j = 0; j < 5; j++) {
-//             if (maze[i][j] == 'S') {
-//                 x = i;
-//                 y = j;
-//             }
-//         }
-//     }
-
-//     mazetr(maze, vis, x, y);
-
-//     return 0;
-// }
+	printf("\n");
+	show_maze(maze); // output maze
+	printf("\nPath to complete maze:\n");
+	create_path(maze, cx, cy);
+	show_path(maze); // output path
+	
+	return 0;
+} // end main()
