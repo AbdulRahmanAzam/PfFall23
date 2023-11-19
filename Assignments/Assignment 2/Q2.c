193b7ccda8cce4c58d6222db9b16e3422a884d84
Abdul Rahman Azam <69141440+AbdulRahmanAzam@users.noreply.github.com>
2023-11-19 19:33:07 +0500

    Update Q2.c

--- Assignments/Assignment 2/Q2.c
+++ Assignments/Assignment 2/Q2.c
@@ -1,37 +1,79 @@
-/*Name : Abdul Rahman Azam 
-Roll no : 23k-0061
-Date : 3-Nov-2023
-Discription : find largest square sub matrix of 1's
+/*Name: Abdul Rahman Azam 
+* Roll no: 23k-0061
+* Date: 3-Nov-2023
+* Description: find the largest square submatrix of 1's
 */
 
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-int main(){
-    int count=0,b=0,c=2;
-    int x=0,y=0;
-    int arr[4][4] = {
-        {1,0,1,1},
-        {0,0,1,0},
-        {1,1,1,1},
-        {1,0,1,1},
-    };
-    int i,j,k,l;
-    int brr[2][2];
-    
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
-            count=0;
-            for(k=i;k<i+2;k++){
-                for(l=j;l<j+2;l++){
-                    if(arr[k][l])count++;
-                }
-            }
-            if(count==4){
-               x=i;
-               y=j;
+
+void show_matrix(int mat[][100], int n, int m)
+{
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) printf("%-*d", n, mat[i][j]);
+        printf("\n");
+    }
+} // end show_matrix(int [][100], int, int)
+
+void input_matrix(int mat[][100], int n, int m)
+{
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("Enter element [%d][%d]: ", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+        printf("\n");
+    }
+} // end input_matrix(int [][100], int, int)
+
+int sum_submatrix(int mat[][100], int i, int j, int sq) {
+    int sum = 0;
+    for (int n = i;n < sq + i; n++) for (int m = j;m < sq + j; m++) sum += mat[n][m];
+
+    return sum;
+} // end sum_subarray(int [][100], int, int, int)
+
+int find_largest_square(int mat[][100], int n, int m)
+{   
+    int sq = (n > m) ? m : n;
+
+    // keep iterating until less than 2
+    while (sq >= 2) {
+
+        // for each sq x sq matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                // check if sum of all the elements in the subarray is equal to the square of sq
+                if (sq + i <= n && sq + j <= m && sum_submatrix(mat, i, j, sq) == sq*sq) return sq;
             }
         }
+
+        // reducing size of submatrix
+        sq--;
     }
-    printf("row = %d - %d \ncolumn = %d - %d",x,x+2,y,y+2);
-}//end main
+
+    return 0;
+} // end find_largest_square(int [][100], int, int)
+ 
+int main() 
+{
+    int n, m, arr[100][100] = {0};
+
+    // do not accept matrices smaller than 2x2
+    do {
+        printf("Enter dimensions of matrix (NxM): ");
+        scanf("%dx%d", &n, &m);
+    } while (n < 2 || m < 2);
+    
+    input_matrix(arr, n, m);
+    printf("Your matrix:\n");
+    show_matrix(arr, n, m);
+    printf("\n");
+
+    int sq = find_largest_square(arr, n, m);
+
+    // check if subarray found
+    (!sq) ? printf("No square matrix of size >= 2x2 found") : printf("Largest square matrix of size %dx%d found", sq, sq);
+
+    return 0;
+} // end main()
